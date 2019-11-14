62e337e82f0764962c707d473fa6b5f8d695d65e
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2019-11-14 23:54:36 +0100

    Updates

---
+++ precache-manifest.41d021a6505566dec9db54a34a038370.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe360a7ca5219313de41a7f289581fd9",
+    "url": "/plotly-with-cra/index.html"
+  },
+  {
+    "revision": "8613ebf29f26dccf63ce",
+    "url": "/plotly-with-cra/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "b89f2e4a5205022d95c7",
+    "url": "/plotly-with-cra/static/js/2.1a675004.chunk.js"
+  },
+  {
+    "revision": "8613ebf29f26dccf63ce",
+    "url": "/plotly-with-cra/static/js/main.5d76885d.chunk.js"
+  },
+  {
+    "revision": "ff0e7d2e8c34ed8bb400",
+    "url": "/plotly-with-cra/static/js/runtime~main.b9849e01.js"
+  }
+]);
