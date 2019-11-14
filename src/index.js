3a6f81a8ae57db7a29bc4beb9c58804b2870c0ab
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2019-11-14 23:46:35 +0100

    added slider example

--- src/index.js
+++ src/index.js
@@ -2,23 +2,79 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Plot from "react-plotly.js";
 
+import rawData from './data.json'
+
 import "./styles.css";
 
+var xField = 'Date';
+var yField = 'Mean_TemperatureC';
+
+var selectorOptions = {
+    buttons: [{
+        step: 'month',
+        stepmode: 'backward',
+        count: 1,
+        label: '1m'
+    }, {
+        step: 'month',
+        stepmode: 'backward',
+        count: 6,
+        label: '6m'
+    }, {
+        step: 'year',
+        stepmode: 'todate',
+        count: 1,
+        label: 'YTD'
+    }, {
+        step: 'year',
+        stepmode: 'backward',
+        count: 1,
+        label: '1y'
+    }, {
+        step: 'all',
+    }],
+};
+/*
+Plotly.d3.csv(rawDataURL, function(err, rawData) {
+    if(err) throw err;
+
+    var data = 
+    var layout = ;
+
+    Plotly.plot('graph', data, layout);
+});*/
+
+function prepData(rawData) {
+    var x = [];
+    var y = [];
+
+    rawData.forEach(function(datum, i) {
+
+        x.push(new Date(datum[xField]));
+        y.push(datum[yField]);
+    });
+
+    return [{
+        mode: 'lines',
+        x: x,
+        y: y
+    }];
+}
 class App extends React.Component {
   render() {
     return (
       <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: "scatter",
-            mode: "lines+points",
-            marker: { color: "red" }
+        data={prepData(rawData)}
+        layout={{
+          title: 'Time series with range slider and selectors',
+          xaxis: {
+              rangeselector: selectorOptions,
+              rangeslider: {}
           },
-          { type: "bar", x: [1, 2, 3], y: [2, 5, 3] }
-        ]}
-        layout={{ width: 320, height: 240, title: "A Fancy Plot" }}
+          yaxis: {
+              fixedrange: true
+          }
+      }}
       />
     );
   }
